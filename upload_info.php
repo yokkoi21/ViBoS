252babd976b48c16184636b7aadc730712b5aac7
よっこい <32246532+yokkoi21@users.noreply.github.com>
2018-01-28 06:24:41 +0900

    ViBoS Files Upload
    PHP files and HTML files upload.
    This files use at ViBoS WEB pages.
    ViBoS is portal site that provide information of iwate prefectural university.
    But this source code is prototype.

---
+++ upload_info.php
@@ -0,0 +1,24 @@
+<?php
+if(file_exists('./json/request.json')){
+header( "Content-Type: application/json; charset=utf-8" ) ;
+touch('./json/info.json');
+#$json_obj = (array)json_decode(stream_get_contents($fp), true);
+$json = file_get_contents('./json/info.json');
+$json_string = file_get_contents('php://input');
+$obj_json = json_decode($json, true);
+$obj_json_string = json_decode($json_string);
+#echo $json_string;
+$obj_json = $obj_json['data'];
+$obj_json[] = $obj_json_string;
+$records = ['data' => $obj_json,'status' => 0,'msg' => 'Success'];
+$out_json = json_encode($records);
+print_r($out_json);
+#rewind($fp);
+#fwrite(json_encode($records));
+file_put_contents('./json/info.json', $out_json,  LOCK_EX);
+#ftruncate($fp, ftell($fp));
+#fclose($fp);
+}else {
+  echo "データがありません";
+}
+?>
