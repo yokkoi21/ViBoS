8e31fef2e50d107c632571bc679d50f043b9794f
よっこい <32246532+yokkoi21@users.noreply.github.com>
2018-03-08 14:16:25 +0900

    Update upload_request.php

--- upload_request.php
+++ upload_request.php
@@ -1,23 +1,17 @@
 <?php
 if(file_exists('./json/request.json')){
-header( "Content-Type: application/json; charset=utf-8" ) ;
-touch('./json/request.json');
-#$json_obj = (array)json_decode(stream_get_contents($fp), true);
-$json = file_get_contents('./json/request.json');
-$json_string = file_get_contents('php://input');
-$obj_json = json_decode($json, true);
-$obj_json_string = json_decode($json_string);
-#echo $json_string;
-$obj_json = $obj_json['data'];
-$obj_json[] = $obj_json_string;
-$records = ['data' => $obj_json,'status' => 0,'msg' => 'Success'];
-$out_json = json_encode($records);
-print_r($out_json);
-#rewind($fp);
-#fwrite(json_encode($records));
-file_put_contents('./json/request.json', $out_json,  LOCK_EX);
-#ftruncate($fp, ftell($fp));
-#fclose($fp);
+  header( "Content-Type: application/json; charset=utf-8" ) ;
+  touch('./json/request.json');
+  $json = file_get_contents('./json/request.json');
+  $json_string = file_get_contents('php://input');
+  $obj_json = json_decode($json, true);
+  $obj_json_string = json_decode($json_string);
+  $obj_json = $obj_json['data'];
+  $obj_json[] = $obj_json_string;
+  $records = ['data' => $obj_json,'status' => 0,'msg' => 'Success'];
+  $out_json = json_encode($records);
+  print_r($out_json);
+  file_put_contents('./json/request.json', $out_json,  LOCK_EX);
 }else {
   echo "データがありません";
 }
